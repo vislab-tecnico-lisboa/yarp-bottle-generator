3893fd0e9e343f1fc311a117ecaf42daf3b8cbbe
plinioMoreno <plinio@isr.tecnico.ulisboa.pt>
2015-11-13 15:13:32 +0000

    fixed missing math.h header

--- src/commonbeginninggenerator.cpp
+++ src/commonbeginninggenerator.cpp
@@ -14,7 +14,8 @@ std::string CommonBeginningGenerator::generateCode() {
   std::string code;
 
   code += "#include <iostream>\n";
-  code += "#include <yarp/os/all.h>\n\n";
+  code += "#include <yarp/os/all.h>\n";
+  code += "#include <math.h>\n\n";
   code += "using namespace yarp::os;\n\n";
   code += "int main(int argc, char *argv[]) {\n";
   code += "  Network yarp;\n\n";
